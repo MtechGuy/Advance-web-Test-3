0517511ab3e917d0f9b52039f2fa10d3469c5db8
Alex H. Peraza <2020152022@ub.edu.bz>
2024-11-16 13:39:01 -0600

    completed implementation for books

--- migrations/000004_create_tokens_table.up.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS tokens (
-    hash bytea PRIMARY KEY,
-    user_id bigint NOT NULL REFERENCES users ON DELETE CASCADE,
-    expiry timestamp(0) WITH TIME ZONE NOT NULL,
-    scope text NOT NULL
-);
-
-
